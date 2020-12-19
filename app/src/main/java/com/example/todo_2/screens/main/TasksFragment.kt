c7bff3fec87931683c35e83affc90ad08669cd86
”Timur <timrud89@gmail.com>
2020-12-19 17:23:45 +0300

    123

---
+++ app/src/main/java/com/example/todo_2/screens/main/TasksFragment.kt
@@ -0,0 +1,43 @@
+package com.example.todo_2.screens.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todo_2.R
+import kotlinx.android.synthetic.*
+
+class TasksFragment : Fragment(R.layout.fragment_content_main) {
+    private lateinit var v: View
+    private var recyclerView: RecyclerView? = null
+    private val adapter = Adapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        v = inflater.inflate(R.layout.fragment_content_main, container, false)
+        recyclerView = v.findViewById(R.id.list)
+        recyclerView!!.layoutManager =
+            LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+        recyclerView!!.adapter = adapter
+        return v
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        mainViewModel.taskTodayLiveData.observe(this, { tasks ->
+            adapter.setItems(tasks)
+        })
+
+    }
+
+}
